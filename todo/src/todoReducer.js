192c23e712a99606f2b0bb250cbaf802ff656db8
Bhavik Ravani <ravanibhavik@gmail.com>
2018-04-18 23:59:49 -0400

    Fixed Dup Todos Bug.

--- todo/src/todoReducer.js
+++ todo/src/todoReducer.js
@@ -1,14 +1,15 @@
 import { ADD_TODO, TOGGLE_TODO, DELETE_TODO } from './todoAction';
 import { v4 } from 'uuid';
 
-const storage = JSON.parse(localStorage.getItem('todos')) || [];
+const localTodos = localStorage.getItem('todos');
+const initialState = localTodos === undefined ? [] : JSON.parse(localTodos);
 
-const addToLocalStorage = newTodo => {
-  storage.push(newTodo)
-  localStorage.setItem('todos', JSON.stringify(storage))
+const addToLocalStorage = newState => {
+  // storage.push(newTodo)
+  localStorage.setItem('todos', JSON.stringify(newState))
 }
 
-const todoReducer = (state = storage, action) => {
+const todoReducer = (state = initialState, action) => {
   switch (action.type) {
   case ADD_TODO:
     const newTodo = {
@@ -16,7 +17,7 @@ const todoReducer = (state = storage, action) => {
       completed: false,
       id: v4()
     }
-    addToLocalStorage(newTodo)
+    addToLocalStorage([...state, newTodo])
     return [...state, newTodo]
   case TOGGLE_TODO:
     return state.map(todo => (
