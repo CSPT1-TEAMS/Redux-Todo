d47259700d3fa8dc5b05e50de94300f03d017e8a
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-04-21 16:05:27 -0500

    Typing everything in by hand, going over the code...

--- todo/src/Todos.js
+++ todo/src/Todos.js
@@ -12,4 +12,29 @@ class Todos extends Component {
     inputChange = (e) => {
         this.setState({todo: e.target.value })
     }
+
+    addTodo = () => {
+        const { todo } = this.state;
+        this.props.addTodoToState(todo);
+        this.setState({ todo: ''});
+    }
+    render() {
+        return (
+            <div>
+                <span>Add Todo</span>
+                <input type="text" value={this.state.todo} onChange={this.inputChange} />
+                <button onClick={this.addTodo}>Submit</button>
+                <h2>Todos:</h2>
+                <ul>
+                    {this.props.todos.map( (todo, index)=> {
+                        const style = {
+                            textDecoration: todo.completed ? 'line-through' : 'none'
+                        }
+                        return <li key={index} style={style} onClick={() => {this.props.toggleTodoOnState(index, !todo.completed)}}>{todo.value}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
 }
+
