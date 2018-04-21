403ebc4833187cc2e2d397f45e5e7407be598f16
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-04-21 16:13:06 -0500

    Rubber ducking my way through the solution.

---
+++ todo/src/actions/index.js
@@ -0,0 +1,7 @@
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+
+export const addTodoToState = (todo) => {
+    const todoObj = { value: todo, completed: false };
+    
+}
